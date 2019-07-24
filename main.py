e813f8084928176437cf17296c27f1332404c2b3
Richard Beattie <richard.beattie91@gmail.com>
2019-07-24 13:57:42 -0400

    Fixed issue of looking at velocity instead of pressure

--- main.py
+++ main.py
@@ -12,18 +12,17 @@ import math
 ##GLOBALS
 tol = 1e-6
 iterations = 0
-times = []
-its = []
-times_pre = []
-its_pre = []
-max_fill = 100000
-drop_step = 0.00001
+fill_step = 1
+drop_step = 0.1
 
-f = open("a_array", "rb")
+pre_its = []
+pre_times = []
+
+f = open("a_array_164", "rb")
 matrix = pickle.load(f)
 f.close()
 
-f = open("b_array", "rb")
+f = open("b_array_164", "rb")
 solution = np.load(f)
 f.close()
 
@@ -33,38 +32,30 @@ def count_iterations(result):
     iterations += 1
 
 def create_preconditioner(A, fill_factor=10, drop_tol=1e-4):
-    iLU = spilu(A, fill_factor=fill_factor, drop_tol=drop_tol)
-    # iLUx = lambda x: iLU.solve(x)
-    # P = LinearOperator(matrix.shape, iLUx)
-    return iLU
+    iLU = spilu(A.tocsc(), fill_factor=fill_factor, drop_tol=drop_tol)
+    iLUx = lambda x: iLU.solve(x)
+    P = LinearOperator(matrix.shape, iLUx)
+    return P
 
-# if __name__ == "__main__":
-#     for i in range(1, 1000):
-#         iterations = 0
-#         P = create_preconditioner(matrix, fill_factor=i)
-#         start = time.time()
-#         (results, info) = lgmres(matrix, solution, tol=tol, atol=tol, M=P, callback=count_iterations)
-#         elapsed = time.time() - start
-#         times_pre.append(elapsed)
-#     plt.plot(range(1,1000), times_pre)
-#     plt.show()
-
-
-## Creates graph of magnitude of values in
-mags = []
+fill_range = np.arange(1, 10, fill_step)
 
 if __name__ == "__main__":
-    for i in range(1, 1000):
-        print(i)
-        av = []
-        P = create_preconditioner(matrix, fill_factor=i)
-        inverse = P.L.A.dot(P.U.A)
-        identity = matrix.dot(inverse)
-        for i in range(1, 500):
-            row = random.sample(range(0, identity.shape[0]), 1)
-            column = random.sample(range(0, identity.shape[1]), 1)
-            av.append(identity[row,column][0])
-        mag = math.log10(np.mean(av))
-        mags.append(mag)
-    plt.plot(range(1, 1000), mags)
+    for i in fill_range:
+        iterations = 0
+        try:
+            print("Starting with i value of", i) 
+            P = create_preconditioner(matrix, fill_factor=i)
+            start = time.time()
+            (result, info) = lgmres(matrix, solution, tol=tol, atol=tol, M=P, callback=count_iterations)
+            elapsed = time.time() - start
+            pre_its.append(iterations)
+            pre_times.append(elapsed)
+            print("Finished drop_step step", i, "in", elapsed, "with", iterations, "iterations")
+        except RuntimeError:
+            print("Runtime error")
+            pre_its.append(np.nan)
+            pre_times.append(np.nan)
+    plt.plot(fill_range, pre_its)
+    plt.show()
+    plt.plot(fill_range, pre_times)
     plt.show()
